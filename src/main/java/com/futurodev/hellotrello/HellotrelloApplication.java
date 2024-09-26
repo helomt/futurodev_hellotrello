9b4882860b3f1fb141c81e012cb33240af4dc74c
Heloisa Tavares <heloisamt@gmail.com>
2024-09-26 20:25:26 -0300

    :tada: First Commit

---
+++ src/main/java/com/futurodev/hellotrello/HellotrelloApplication.java
@@ -0,0 +1,13 @@
+package com.futurodev.hellotrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellotrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellotrelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futurodev/hellotrello/HellotrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurodev.hellotrello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellotrelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
